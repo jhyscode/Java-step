e472f5246c7398d4e8c7aafe48257d4a874f254e
e <1770613507@qq.com>
2022-10-20 17:11:25 +0800

    add dev

---
+++ src/com/allen/Main.java
@@ -0,0 +1,12 @@
+package com.allen;
+
+/**
+ * @author ：jhys
+ * @date ：Created in 2022/10/20 17:10
+ * @Description ：
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
