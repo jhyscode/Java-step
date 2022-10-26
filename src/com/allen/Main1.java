f0850f64b3adac06e047bf3ec787f62a8b5bc96a
e <1770613507@qq.com>
2022-10-26 10:34:52 +0800

    master更新

---
+++ src/com/allen/Main1.java
@@ -0,0 +1,12 @@
+package com.allen;
+
+/**
+ * @author ：jhys
+ * @date ：Created in 2022/10/26 10:33
+ * @Description ：
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+}
